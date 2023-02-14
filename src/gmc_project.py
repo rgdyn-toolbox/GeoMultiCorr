741a0ce2bd550d9821b2be9d1dbaae98a0fe163d
duvanelt <thibaut.duvanel@univ-smb.fr>
2023-02-14 18:43:20 +0100

    updated and get + pzones

--- src/gmc_project.py
+++ src/gmc_project.py
@@ -3,12 +3,12 @@ import os
 import re
 from pathlib import Path
 import geopandas as gpd
-import gmc_thumb
+import gmc_thumb as gmc_th
+import gmc_pzone as gmc_pz
+import gmc_pair as gmc_pa
 import pandas as pd
-from telenvi import raster_tools as rt
 
 ROOT_TEMPLATE = Path(__file__).parent.with_name('template')
-THUMBNAME_PATTERN = '^([a-z]|[A-Z]|-)+_[0-9]{4}(-[0-9]{2}){2}_.*.(tif|TIF)$'
 
 def is_conform_to_gmc_template(target_root_path):
     for thing in os.listdir(ROOT_TEMPLATE):
@@ -17,7 +17,7 @@ def is_conform_to_gmc_template(target_root_path):
     return True
 
 class GMC_Project:
-    
+
     def __init__(self, target_root_path : str):
         target_root_path = Path(target_root_path)
 
@@ -34,145 +34,128 @@ class GMC_Project:
 
         # Si elle n'existe pas : on la créée
         else:
-            print('copie')
-            print(target_root_path)
             os.system(f"cp -r {ROOT_TEMPLATE} {target_root_path}")
 
-        self.root_path = Path(target_root_path)
-        self.geodb_path = os.path.join(target_root_path, 'database.gpkg')
-        self.pz = gpd.read_file(self.geodb_path, layer='Pzones').apply(lambda x: x.str.lower(), 1)
-        self.th = gpd.read_file(self.geodb_path, layer='Thumbs')
-        self.th['GMC_Thumb'] = self.th.apply(lambda x: gmc_thumb.GMC_Thumb(x), axis=1)
-        self.pa = gpd.read_file(self.geodb_path, layer='Pairs').apply(lambda x: x.str.lower(), 1)
-        self.ge = gpd.read_file(self.geodb_path, layer='Geomorphs').apply(lambda x: x.str.lower(), 1)
+        self.p_root = str(target_root_path)
+        self.p_raster_data = str(Path(self.p_root, 'raster_data'))
+        self.p_geodb = os.path.join(target_root_path, 'database.gpkg')
+        self._pzones = gpd.read_file(self.p_geodb, layer='Pzones')
+        self._thumbs = gpd.read_file(self.p_geodb, layer='Thumbs')
+        self._pairs  = gpd.read_file(self.p_geodb, layer='Pairs')
+        self.pz_names = list(self._pzones.pz_name.unique())
 
-    def backup_db(self):
-        backup_path = Path(self.root_path, 'backup_db.gpkg')
-        os.system(f"cp -r {self.geodb_path} {backup_path}")
-        return len(gpd.read_file(self.geodb_path)) == len(gpd.read_file(backup_path))
+    ########### GETTERS ###########
 
-    def get_thumbs_infos(self, criterias):
+    def get_thumbs_overview(self, criterias=''):
+        """Send a dataframe with informations about each thumb raster file meeting the criterias.
+           If there's no criterias, send informations about all the thumbs of the project """
+        return self.search_engine('Thumbs', criterias)
+
+    def get_thumbs(self, criterias=''):
+        """Send a list of GMC_Thumb objects meeting the criterias"""
+        selected_thumbs = self.get_thumbs_overview(criterias)
+        return [gmc_th.GMC_Thumb(x.th_path) for x in selected_thumbs.iloc]
+
+    def get_pairs_overview(self, criterias=''):
+        """Send a dataframe with each possible Pair according to the Thumbs"""
+        return self.search_engine('Pairs', criterias)
+    
+    def get_pairs(self, criterias=''):
+        selected_pairs = self.get_pairs_overview(criterias)
+        return [gmc_pa.GMC_Pair(self, target_path = x.pa_path) for x in selected_pairs.iloc]
+
+    def get_pzones_overview(self, pz_name=''):
+        return self.search_engine('Pzones', pz_name)
+
+    def get_pzones(self, pz_name=''):
+        selected_pzones = self.get_pzones_overview(pz_name)
+        return [gmc_pz.GMC_Pzone(x.pz_name, self) for x in selected_pzones.iloc]
+
+    ###############################
+
+    def copy_geodb(self):
+        """quickly create a copy of the project geopackage named backup.gpkg"""
+        backup_path = Path(self.p_root, 'backup_geodb.gpkg')
+        os.system(f"cp -r {self.p_geodb} {backup_path}")
+        return len(gpd.read_file(self.p_geodb)) == len(gpd.read_file(backup_path))
+
+    ########### SETTERS ###########
+
+    def update_thumbs(self):
+        """add or remove rows in Thumbs layer, according to the thumbs stored in the project"""
+
+        # Copy the database before the transaction
+        assert self.copy_geodb()
+
+        # Get 2 version of the Thumbs layer
+        opt_root = Path(self.p_raster_data)
+        old = self._thumbs
+        new = gpd.GeoDataFrame([gmc_th.GMC_Thumb(target_path).to_pdserie() for target_path in filter(lambda x: gmc_th.THUMBNAME_PATTERN.match(x.name), list(opt_root.glob(pattern='**/opticals/*.tif')))])
+
+        # Comparison
+        common = new.merge(old, on=['th_path'])
+        stables = old[old.th_path.isin(common.th_path)]
+        addeds = new[~new.th_path.isin(common.th_path)]
+
+        # Push it into the geodatabase
+        updated = pd.concat([stables, addeds])
+        updated.to_file(self.p_geodb, layer='Thumbs')
+        return updated
+
+    def update_pairs(self):
+        """add or remove rows in Pairs layer, according to the thumbs stored in the project"""
+
+        # Copy the database before the transaction
+        assert self.copy_geodb()
+
+        # For each processing zone we
+        updated = []
+        for pz in self.get_pzones():
+            pairs = pz.get_pairs_overview()
+            [updated.append(pa) for pa in pairs.iloc()]
+
+        # Push it into the geodatabase        
+        updated = gpd.GeoDataFrame(updated).set_crs(epsg=2154)
+        updated.to_file(self.p_geodb, layer='Pairs')
+        return updated
+
+    ###############################
+
+    def search_engine(self, layername, criterias=''):
+        """A search engine among project layers"""
+
+        # if there is only one criteria we store it in a list
         if type(criterias) in (str, int):
             criterias = [criterias]
+
+        # add an "and" statement between each criteria
         pattern = ""
         for c in criterias:
             if type(c) == int:
                 c = str(c)
             pattern += f"(?=.*{c.lower()})"
-        lower_th = self.th.apply(lambda x: x.str.lower(), 1)
-        return self.th[lower_th.th_path.str.contains(re.compile(pattern))]
+        pattern = re.compile(pattern)
 
-    def get_gmc_thumbs(self, criterias):
-        thumbs_infos = self.get_thumbs_infos(criterias)
-        gmc_thumbs = list(thumbs_infos.GMC_Thumb)
-        return gmc_thumbs
-
-    def get_gdf_from_physical_thumbs(self):
-
-        """
-        Retourne un GeoDataFrame à partir des fichiers
-        rasters de vignettes tels qu'enregistrés sur le disque dur
-        """
-
-        opt_root = Path(self.root_path, 'raster_data')
-        thumbs = []
-
-        # Pour tous les fichiers tifs du dossier raster_data qui correspondent au pattern THUMBNAME_PATTERN
-        for targetpath in filter(lambda x: re.compile(THUMBNAME_PATTERN).match(x.name), list(opt_root.glob(pattern='**/opticals/*.tif'))):
-
-            # On récupère des métadonnées sur cette vignette
-            th_path = str(targetpath)
-            th_key = targetpath.name.split('.')[0]
-            th_pz, th_date, th_sensor = th_key.split('_')
-            th_year = int(th_date.split('-')[0])
-            th_geom = rt.drawGeomExtent(th_path, geomType='shly')
-            th_valid=0
+        match layername:
+            case 'Thumbs':
+                normal_th = self._thumbs
+                lower_th  = normal_th.apply(lambda x: x.str.lower(), 1)
+                return normal_th[lower_th.th_path.str.contains(pattern)]
             
-            # On écrit tout ça dans une pd_serie
-            th_serie = pd.Series({
-                'th_path'   : th_path,
-                'th_sensor' : th_sensor,
-                'th_date'   : th_date,
-                'th_year'   : th_year,
-                'th_valid'  : th_valid,
-                'pz_name'   : th_pz,
-                'geometry'  : th_geom
-            })
-
-            thumbs.append(th_serie)
-
-        return gpd.GeoDataFrame(thumbs)
-    
-    def update_thumbs_in_geodb_from_physical(self):
-
-        """
-        Met à jour la table Thumbs en fonction
-        de l'état des fichiers rasters sur le disque
-        """
-
-        # Copy the database before the transaction
-        assert self.backup_db()
-
-        # Get 2 version of Thumbs
-        ths_phy = self.get_gdf_from_physical_thumbs()
-        ths_gdb = self.th.drop(labels='GMC_Thumb', axis=1)
-
-        # Comparison
-        common = ths_phy.merge(ths_gdb, on=['th_path'])
-        ths_stables = ths_gdb[ths_gdb.th_path.isin(common.th_path)]
-        ths_addeds = ths_phy[~ths_phy.th_path.isin(common.th_path)]
-        ths_stables_and_news = pd.concat([ths_stables, ths_addeds])
-
-        # Write new Thumbs table in the geotabase
-        ths_stables_and_news.to_file(self.geodb_path,layer='Thumbs')
-
-        # Update layer instance
-        self.th = ths_stables_and_news
-
-        return ths_stables_and_news        
-
-    def update_pairs_in_geodb_from_thumbs(self):
-
-        """
-        Met à jour la table Pairs en fonction
-        de l'état de la table Thumbs
-        -
-        si on ajoute des vignettes il faut d'abord mettre à jour
-        la table Thumb ave update_thumbs_in_geodb_from_physical
-        """
-
-        # Copy the database before the transaction
-        assert self.backup_db()
-
-        # Create an empty list
-        pairs_infos = []
-
-        # For each processing zone
-        for pz in self.th.pz_name.unique():
-
-            # We get all the thumbs on this pz
-            pz_thumbs = self.get_gmc_thumbs(pz)
-
-            # For all images
-            for left in pz_thumbs:
-
-                # We make a Pair with all others
-                # and we ad it to the list pairs_infos
-                for right in pz_thumbs:
-                    try:
-                        pair = left + right
-                        pairs_infos.append(pair.to_pdserie())
-        
-                    # It's the same image so we skip them
-                    except AssertionError:
-                        continue
-
-        # Here we convert our list into a geoDataFrame
-        new_pairs = gpd.GeoDataFrame(pairs_infos).set_crs(epsg=2154)
-
-        # And we update the geodatabae layer Pairs
-        new_pairs.to_file(self.geodb_path, layer='Pairs')
-        return new_pairs
+            case 'Pairs':
+                normal_pa = self._pairs
+                lower_pa = normal_pa.apply(lambda x: x.str.lower(), 1)
+                return normal_pa[lower_pa.pa_path.str.contains(pattern)]
+            
+            case 'Pzones':
+                pz_layer = self._pzones
+                if criterias!=['']:
+                    requested_pz_name = criterias[0].lower()
+                    return pz_layer[pz_layer.pz_name == requested_pz_name]
+                else:
+                    return pz_layer
 
 p = GMC_Project('/media/duvanelt/TD002/sandbox_gmc/vanoise')
+p.get_pairs_overview()
+p.update_pairs()
 # %%
--- src/gmc_thumb.py
+++ src/gmc_thumb.py
@@ -5,27 +5,24 @@ from telenvi import raster_tools as rt
 import pandas as pd
 import gmc_pair
 
-THUMBNAME_PATTERN = '^([a-z]|[A-Z]|-)+_[0-9]{4}(-[0-9]{2}){2}_.*.(tif|TIF)$'
+THUMBNAME_PATTERN = re.compile('^([a-z]|[A-Z]|-)+_[0-9]{4}(-[0-9]{2}){2}_.*.(tif|TIF)$')
 
 class GMC_Thumb:
 
-    def __init__(self, th_serie):
-
-        target_path = Path(th_serie.th_path)
+    def __init__(self, target_path):
+        target_path = Path(target_path)
 
         # Vérification de l'existence du fichier pointé par l'adresse 
         # et de la validité du nom au regard du pattern défini
         assert target_path.exists(), 'fichier inexistant'
-        assert re.compile(THUMBNAME_PATTERN).match(target_path.name)
+        assert THUMBNAME_PATTERN.match(target_path.name), 'filename don\'t match with gmc_thumbname pattern'
 
-        self.th_path = th_serie.th_path
+        self.th_path = str(target_path)
         self.th_key = target_path.name.split('.')[0]
-        self.th_sensor = th_serie.th_sensor
-        self.th_date = th_serie.th_date
-        self.th_year = th_serie.th_year
-        self.th_valid = th_serie.th_valid
-        self.th_pz_name = th_serie.pz_name
-        self.geometry = th_serie.geometry
+        self.th_pz_name, self.th_date, self.th_sensor = self.th_key.split('_')
+        self.th_year = int(self.th_date.split('-')[0])
+        self.geometry = rt.drawGeomExtent(self.th_path, geomType='shly')
+        self.th_valid=0
 
     def to_pdserie(self):
         return pd.Series({
@@ -38,7 +35,7 @@ class GMC_Thumb:
             'geometry':self.geometry})
 
     def __add__(self, right):
-        return gmc_pair.GMC_Pair(self, right)
+        return gmc_pair.GMC_Pair(left=self, right=right)
 
     def __repr__(self):
         return f"""---------
@@ -50,10 +47,10 @@ sensor : {self.th_sensor}
 """
 
     def get_ds(self):
-        return gdal.Open(self.th_path)
+        return gdal.Open(str(self.th_path))
+
+    def get_geoim(self):
+        return rt.GeoIm.GeoIm(str(self.th_path))
 
     def show(self):
         self.get_geoim().show()
-
-    def get_geoim(self):
-        return rt.GeoIm.GeoIm(self.th_path)
