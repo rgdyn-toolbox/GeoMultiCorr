859c7e6a5094468025f07259f53fb8057721bd7a
cusicand <diego.cusicanqui.vg@gmail.com>
2026-05-11 16:50:01 +0200

    set up version 0.1.0 o __init__.py

--- geomulticorr/__init__.py
+++ geomulticorr/__init__.py
@@ -1,7 +1,7 @@
-version = 'developer version'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+version = "0.1.0"
 
-print(f'''-------------
-geomulticorr {version}
--------------''')
+print(f"geomulticorr {__version__}")
 
 from geomulticorr.core.session import open_gmc_session
