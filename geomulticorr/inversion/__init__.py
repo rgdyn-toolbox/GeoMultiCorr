42d7da0a5bdcbfaf05a85098cc1f5bce6a6de703
cusicand <diego.cusicanqui.vg@gmail.com>
2026-06-04 11:47:26 +0200

    update: import inversion modules

--- geomulticorr/inversion/__init__.py
+++ geomulticorr/inversion/__init__.py
@@ -26,3 +26,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # ---------------------------------------------------------------------------- #
+from geomulticorr.inversion.tio_inversion import TIOInversion, tiff2bin
