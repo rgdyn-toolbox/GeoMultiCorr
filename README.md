cbdd45108386677a3499ce1c0c998e6bfcac94b7
Thibaut Duvanel <thibaut.duvanel@unil.ch>
2026-09-11 10:41:04 +0200

    Initial Paper Branch Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# GeoMultiCorr
-A python lib to study earth-surface displacements from optical images with Ames Stereo Pipeline
--- src/geomulticorr/__init__.py
+++
@@ -1,7 +0,0 @@
-version = 'developer version'
-
-print(f'''-------------
-geomulticorr {version}
--------------''')
-
-from geomulticorr.session import Open
